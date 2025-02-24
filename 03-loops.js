953e5c2639d6d527ec88ea5e87e20ee4ee1d3a1c
Sriram R <sriram.r.codes@gmail.com>
2025-02-24 21:30:17 +0530

    js-basics

---
+++ 03-loops.js
@@ -0,0 +1,28 @@
+// loops
+
+// 1. for loop
+//  Syntax:
+//  for(initialization; condition; increment/decrement) {
+//      // code
+//  }
+
+//  Example:
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+// 2. while loop
+//  Syntax:
+
+//  while(condition) {
+//      // code
+//  }
+
+//  Example:
+
+let j = 0;
+while (j < 5) {
+    console.log(j);
+    j++;
+}
