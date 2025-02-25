4f96fc87e3e987e69fdf882072b8b31d54d2ea1e
Sriram R <sriram.r.codes@gmail.com>
2025-02-25 23:28:52 +0530

    js-basics

---
+++ 04-controlstatements.js
@@ -0,0 +1,29 @@
+// if conditions
+
+// if (condition) {
+//     // code
+// }
+
+//example
+
+let age = 18;
+if (age >= 18) {
+    console.log("You can vote");
+}
+
+// if else conditions
+
+// if (condition) {
+//     // code
+// } else {
+//     // code
+// }
+
+//example
+
+if (age >= 18) {
+    console.log("You can vote");
+}
+else {
+    console.log("You can't vote");
+}
