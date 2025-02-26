deb6120d3349cfe683958b816e304bd2a0103b00
Sriram R <sriram.r.codes@gmail.com>
2025-02-26 23:53:29 +0530

    js-basics

---
+++ 05-Arrays.js
@@ -0,0 +1,32 @@
+// arrays
+
+// 1. Create an array of numbers
+// 2. Create an array of strings
+// 3. Create an array of mixed data types
+// 4. Create an array of arrays
+// 5. Access an array element
+
+// 1. Create an array of numbers
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+
+// 2. Create an array of strings
+let strings = ["apple", "banana", "cherry"];
+console.log(strings);
+
+// 3. Create an array of mixed data types
+let mixed = [1, "apple", true];
+console.log(mixed);
+
+// 4. Create an array of arrays
+
+let arrays = [[1, 2, 3], ["apple", "banana", "cherry"], [true, false]];
+console.log(arrays);
+
+// 5. Access an array element
+
+console.log(numbers[0]);
+console.log(strings[1]);
+console.log(mixed[2]);
+console.log(arrays[1][0]);
+
